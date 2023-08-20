ef801e1eafe0bb50928b42c8998bffb96febd756
siddharth200119 <siddramnani@gmail.com>
2023-08-20 15:33:07 +0530

    mongodb code added to connect to server and insert values

--- index.js
+++ index.js
@@ -7,10 +7,38 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.set('views', path.join(__dirname, '/views'));
 
+//connecting to database
+
+mongoose.connect("mongodb://127.0.0.1:27017/testDB")
+
+//creating schema
+
+const testSchema = new mongoose.Schema({
+    col1: String,
+    col2: Number,
+});
+
+//creating mongoose model
+
+const Test = mongoose.model("Test", testSchema); //here in mongoose the first parameter has to be singular and will be converted to prural and form a collection in our database
+
+const test = new Test({
+    col1: "first col",
+    col2: 2
+});
+
+//save the entry
+
+test.save();
+
+//port for server
+
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
+
+//starting server
 
 app.listen(port, function (){
     console.log("server started");
